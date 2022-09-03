707ec22e0a2fab278c094959e0c8d514db8cb3f9
Peter Calabrese <peter@calabrese.tech>
2022-09-02 20:03:24 -0400

    added colors to extend

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        Malachite: "#04CD1E",
+        "Selective-Yellow": "#FFBC00",
+        "Sunset-Orange": "#FF524E",
+        "border-bar": "#3C3C3C",
+        "left-border-bar": "#333333",
+        "file-panel": "#252526",
+        "react-logo": "#61DBFB",
+        "file-text": "#CCCCCC",
+        "html-logo": "#E44D27",
+        "css-logo": "#214CE5",
+      },
+    },
   },
   plugins: [],
 };
