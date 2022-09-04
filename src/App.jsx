a330f1844876ef738737a1bf4a283108bdcdbd03
Peter Calabrese <peter@calabrese.tech>
2022-09-04 11:17:15 -0400

    added webpage to render in text edit area

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import FileList from './components/FileList';
 import TopBar from './components/TopBar';
 import SideBar from './components/SideBar';
+import WebpageRenderer from './components/WebpageRenderer';
+
+import { useState } from 'react';
 function App() {
+  const [renderPage, setRenderPage] = useState('Home');
+
   return (
     <div>
       <TopBar />
       <div className='flex'>
         <SideBar />
-        <FileList />
+        <FileList setFileToRender={setRenderPage} />
+        <div className='h-screen bg-text-editor-bg w-full'>
+          <WebpageRenderer value={renderPage} />
+        </div>
       </div>
-      
+
     </div>
   )
 }
--- src/components/DirectoryFile.jsx
+++ src/components/DirectoryFile.jsx
@@ -1,9 +1,9 @@
-const DirectoryFile = ({ icon, filename }) => {
+const DirectoryFile = ({ icon, filename, onClick }) => {
     return (
-        <div className="flex justify-start items-center mt-1">
+        <button className="w-full flex justify-start items-center mt-1 hover:bg-slate-200 hover:bg-opacity-10 focus:bg-blue-900" onClick={onClick}>
             {icon}
             <p className='text-file-text text-sm'>{filename}</p>
-        </div>
+        </button>
     );
 }
 
