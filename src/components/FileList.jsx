ec2b0567afd7a5395301d178843c1ce2a4134574
Peter Calabrese <peter@calabrese.tech>
2022-09-04 11:16:35 -0400

    updated file list to allow user to interact with files

--- src/components/FileList.jsx
+++ src/components/FileList.jsx
@@ -1,25 +1,32 @@
-import { VscChevronDown, VscJson } from 'react-icons/vsc'
+import { VscChevronDown, VscJson, VscEllipsis } from 'react-icons/vsc'
 import { SiCss3, SiReact, SiHtml5 } from "react-icons/si";
 import DirectoryFile from './DirectoryFile'
 
-const FileList = () => {
-    // const [selectedFile, setSelectedFile] = useState('Hompage.jsx')
+const FileList = ({ setFileToRender }) => {
+
+    const fileClicked = (toRender) => {
+        setFileToRender(toRender)
+    }
     return (
-        <div className='mr-10 bg-file-panel h-screen w-60 pt-1 pl-1'>
+        <div className=' bg-file-panel h-screen w-60 pt-1 pl-1'>
+            <div className='text-white flex items-center justify-between mt-2 mb-3'>
+                <h2 className='ml-5 text-xs opacity-60'>EXPLORER</h2>
+                <VscEllipsis className="mr-3" size={15} />
+            </div>
             <div className='text-white flex justify-start items-center'>
                 <VscChevronDown size={15} />
                 <h2 className='ml-1 text-xs font-bold'>CALABRESE.TECH</h2>
             </div>
             {/* Directories */}
-            <div className="ml-2">
+            <div className="pl-2">
                 {/* Homepage */}
-                <DirectoryFile icon={<SiReact className='text-react-logo mr-2' size={12} />} filename={"Homepage.jsx"} />
+                <DirectoryFile icon={<SiReact className='text-react-logo mr-2' size={12} />} filename={"Homepage.jsx"} onClick={() => fileClicked('Home')} />
                 {/* About */}
-                <DirectoryFile icon={<SiHtml5 className='text-html-logo mr-2' size={12} />} filename={"About.html"} />
+                <DirectoryFile icon={<SiHtml5 className='text-html-logo mr-2' size={12} />} filename={"About.html"} onClick={() => fileClicked('About')} />
                 {/* Contact */}
-                <DirectoryFile icon={<SiCss3 className='text-css-logo mr-2' size={12} />} filename={"About.html"} />
+                <DirectoryFile icon={<SiCss3 className='text-css-logo mr-2' size={12} />} filename={"Contact.css"} onClick={() => fileClicked('Contact')} />
                 {/* Projects */}
-                <DirectoryFile icon={<VscJson className='text-JSON-logo mr-2' size={12} />} filename={"Projects.json"} />
+                <DirectoryFile icon={<VscJson className='text-JSON-logo mr-2' size={12} />} filename={"Projects.json"} onClick={() => fileClicked('Projects')} />
 
             </div>
         </div>
--- src/components/MenuButton.jsx
+++ src/components/MenuButton.jsx
@@ -1,6 +1,6 @@
 const MenuButton = ({ color, children }) => {
     return (
-        <div className={`${color} h-4 w-4 mx-1 rounded-full my-1 flex justify-center items-center group hover:cursor-pointer`}>
+        <div className={`${color} h-3 w-3 mx-1 rounded-full my-1 flex justify-center items-center group hover:cursor-pointer`}>
             <p className="opacity-0 group-hover:opacity-100 text-xs font-semibold">{children}</p>
         </div>
 
