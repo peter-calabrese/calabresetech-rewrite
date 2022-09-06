d0e6f88c0c0bf525fd299e7bc509e8f57d1af8c7
Peter Calabrese <Peter Calabrese>
2022-09-06 19:20:03 -0400

    added test tabs

--- src/components/VSCComponents/TabBar.jsx
+++ src/components/VSCComponents/TabBar.jsx
@@ -1,7 +1,10 @@
 const TapBar = () => {
     return (
-        <div className="bg-white h-8">
-            <button className="bg-blue-900 h-full">Test txt</button>
+        <div className="bg-tab-bg h-8">
+            <button className="bg-tab-open-focused h-full px-4 text-tab-text-focused">Test txt</button>
+            <button className="bg-tab-open-unfocused h-full px-4 text-tab-text-unfocused">Test txt</button>
+            <button className="bg-tab-open-unfocused h-full px-4 text-tab-text-unfocused">Test txt</button>
+            <button className="bg-tab-open-unfocused h-full px-4 text-tab-text-unfocused">Test txt</button>
         </div>
 
     )
