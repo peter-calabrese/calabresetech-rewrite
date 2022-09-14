01143e74735429f3cef02db18d1c6af181f527ed
Peter Calabrese <peter@calabrese.tech>
2022-09-14 09:04:30 -0400

    updated about page

--- src/components/Webpages/About.jsx
+++ src/components/Webpages/About.jsx
@@ -1,7 +1,8 @@
 const About = () => {
     return (
-        <div className="text-white flex flex-col justify-center items-center h-screen">
-            <h1 className="text-2xl">About Me</h1>
+        <div className="text-white flex flex-col justify-center items-start h-screen pl-10">
+            <h1 className="text-4xl pb-2 font-bold">About Me</h1>
+            <p className="text-2xl mr-[80rem]">I am a full stack developer who has a great mind for solving problems of all different calibres. Creating solutions such as <a className="text-Sunset-Orange" href="https://github.com/peter-calabrese/rtxbot">my RTX stock monitoring bot</a> to helping out friends who need <a className="text-Sunset-Orange" href="https://luciafootwearco.vercel.app">a new website for their company</a>. Learning new frameworks and languages is also something I enjoy! The challange of learning something new, coupled with the ability of being able to walk away from a project with more knowledge than when I started, creates a level of excitement and enjpyment in what I do.</p>
         </div>
     );
 }
