73961e62c048ba0862e7215246234b3584d82141
Peter Calabrese <peter@calabrese.tech>
2022-09-30 13:54:03 -0400

    added suspense fallback for canvas

--- src/components/Webpages/About.jsx
+++ src/components/Webpages/About.jsx
@@ -1,6 +1,6 @@
 import { Canvas, useFrame } from '@react-three/fiber'
 import { TrackballControls } from '@react-three/drei'
-import { useRef } from 'react'
+import { useRef, Suspense } from 'react'
 import Cloud from '../Cloud'
 const About = () => {
 
@@ -11,11 +11,13 @@ const About = () => {
                 <p className="text-2xl ">I am a full stack developer who has a great mind for solving problems of any calibre. I have created solutions such as <a className="text-react-logo" href="https://github.com/peter-calabrese/rtxbot">an inventory monitoring bot</a> with python to helping out friends get RTX graphics cards at MSRP. Developed <a className="text-react-logo " href="https://luciafootwearco.vercel.app">a ReactJS website</a> for LuciaFootwearCo's rapidly growing buisness. In addtion to creating I thuroughly enjoy learning new frameworks and languages! The challange of being placed outside my comfort zone, coupled with the benefit of being able to apply my newly aquired knowledge on the job or on my next project, gives me the drive to firther improve and apply myself as a developer.</p>
             </div>
             <div className="mr-10 ml-40 h-screen w-[5000px]">
-                <Canvas camera={{ position: [0, 0, 35], fov: 90 }}>
-                    <fog attach="fog" args={['#202025', 0, 90]} />
-                    <Cloud count={5} radius={16} />
-                    <TrackballControls noPan={true} />
-                </Canvas>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Canvas camera={{ position: [0, 0, 35], fov: 90 }}>
+                        <fog attach="fog" args={['#202025', 0, 90]} />
+                        <Cloud count={5} radius={16} />
+                        <TrackballControls noPan={true} />
+                    </Canvas>
+                </Suspense>
             </div>
         </div >
 
